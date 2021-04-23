fc7690d982c829007b8de2dff6b2887c8be7d766
Fredy Papa <fredy.luis.wolf@gmail.com>
2021-04-23 18:02:52 -0500

    feat(general): create new project and edit component with target and currentTarget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tareaClase3';
+  title = 'Diferencia entre target y currentTarget';
+
+  viewTarget(event:Event):void{
+    const obj = event.target as HTMLInputElement;
+    console.log("El evento fue disparado por el objeto "+obj.nodeName);
+    alert("El evento fue disparado por el objeto "+obj.nodeName);
+  }
+
+  viewCurrentTarget(event:Event):void{
+    const obj = event.currentTarget as HTMLInputElement;
+    console.log("El objeto cuyo oyente disparó el evento es "+obj.nodeName);
+    alert("El objeto cuyo oyente disparó el evento es "+obj.nodeName);
+  }
+
 }
+
